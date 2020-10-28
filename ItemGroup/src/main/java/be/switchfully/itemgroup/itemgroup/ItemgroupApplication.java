18467023e9f243612c08cacc94624b3ed2041709
Jeroen De Man <jeroendm26@gmail.com>
2020-10-28 11:34:48 +0100

    added structure ItemGroup

---
+++ ItemGroup/src/main/java/be/switchfully/itemgroup/itemgroup/ItemgroupApplication.java
@@ -0,0 +1,13 @@
+package be.switchfully.itemgroup.itemgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemgroupApplication.class, args);
+	}
+
+}
---
+++ ItemGroup/src/test/java/be/switchfully/itemgroup/itemgroup/ItemgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package be.switchfully.itemgroup.itemgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemgroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
